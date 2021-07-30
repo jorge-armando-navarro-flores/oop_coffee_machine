78dfdd8bdf3ec0143080abd77220f1b5da3605d5
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2021-07-30 16:31:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Coffee machine
+This little script simulates a coffee machine.
+
+## Successful buy
+![successful](screenshots/coffee-machine_successful.png)
+
+## No money
+![No money](screenshots/coffee-machine_no-money.png)
+
+## No sufficient ingredients
+![No sufficient ingredients](screenshots/coffee-machine_no-ingredients.png)
+
+## Shutdown
+![shutdown](screenshots/coffee-machine_off.png)
+
+
---
+++ main.py
@@ -0,0 +1,24 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+coffee_maker = CoffeeMaker()
+menu = Menu()
+money_machine = MoneyMachine()
+items = menu.get_items()
+
+order = input(f"What would you like? ({items}): ")
+
+
+while order != "off":
+
+    if order == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(order)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
+    order = input(f"What would you like? ({items}): ")
